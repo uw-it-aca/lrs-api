8790ffa959774d3a20c0744ed8c2e45ac9893b58
Patrick Michaud <pmichaud@uw.edu>
2017-03-31 12:29:21 -0700

    Putting the urls behind a version

--- lrs_api/urls.py
+++ lrs_api/urls.py
@@ -1,11 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from lrs_api.views import (ProcessStatement, RecentActivities,
                            TotalActivities, TopActivities)
 
 
-urlpatterns = [
+v1_urlpatterns = [
     url(r'statements$', ProcessStatement.as_view(), name="lrs_api_statement"),
     url(r'user/recentactivities$', RecentActivities.as_view()),
     url(r'user/totalactivities$', TotalActivities.as_view()),
     url(r'user/topactivities$', TopActivities.as_view()),
+]
+
+urlpatterns = [
+    url(r'^v1/', include(v1_urlpatterns))
 ]
